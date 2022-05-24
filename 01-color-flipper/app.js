ab471450630e7b4b097fb3416d66aa3b0ab6a8d4
Lu de Oliveira <luludeolive.dev@gmail.com>
2022-05-24 16:40:24 -0300

    setup the files

--- 01-color-flipper/app.js
+++ 01-color-flipper/app.js
@@ -1,1 +1,17 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById('btn')
+const color = document.querySelector('.color')
+
+// change the background color when the button is clicked
+
+btn.addEventListener('click', function() {
+    // get random number between 0 and 3 ( the hardcoded const colors)
+    const randomNumber = 2
+    document.body.style.backgroundColor = colors[randomNumber]
+    color.textContent = colors[randomNumber]
+
+} )
+
+function getRandomNumber() {
+    return
+}
