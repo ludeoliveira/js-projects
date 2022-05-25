e5229b0d8ae2d0cd4f33c9a3df1bdec700523e4e
Lu de Oliveira <luludeolive.dev@gmail.com>
2022-05-25 10:19:39 -0300

    finish the color flpper

--- 01-color-flipper/app.js
+++ 01-color-flipper/app.js
@@ -6,12 +6,12 @@ const color = document.querySelector('.color')
 
 btn.addEventListener('click', function() {
     // get random number between 0 and 3 ( the hardcoded const colors)
-    const randomNumber = 2
+    const randomNumber = getRandomNumber()
     document.body.style.backgroundColor = colors[randomNumber]
     color.textContent = colors[randomNumber]
 
 } )
 
 function getRandomNumber() {
-    return
+    return Math.floor(Math.random() * colors.length)
 }
--- 01-color-flipper/hex.js
+++ 01-color-flipper/hex.js
@@ -1,1 +1,19 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById("btn")
+const color = document.querySelector(".color")
+
+// set up a loop who will generate the 6 values hex color code based on the positions of the hex array. ex: #f15025. the loop will run 6 times and add the random value to the hexColor value.
+
+btn.addEventListener("click" , function() {
+    let hexColor = "#"
+    for (let i = 0; i < 6; i++) {
+        hexColor += hex[getRandomNumber()]
+    }
+    
+    color.textContent = hexColor
+    document.body.style.backgroundColor = hexColor
+})
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * hex.length)
+}
